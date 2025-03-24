6884cccd378e25f9548f9daf8d03a0cafccdae8f
Kirankumardhule <kirankumardhule@gmail.com>
2025-03-25 00:41:41 +0530

    Create aes.py

---
+++ aes.py
@@ -0,0 +1,7 @@
+"""
+AES-128 Implementation
+Author: [Your Name]
+References:
+- NIST FIPS 197 (AES Standard): https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197.pdf
+- Rijndael S-box/MixColumns theory (Daemen & Rijmen)
+"""
